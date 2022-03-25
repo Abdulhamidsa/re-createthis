294a2e9a12d3b87ccc29710824c69475ff92fce3
Jonas Holbech <abdu3793@stud.kea.dk>
2022-03-25 13:19:08 +0100

    DYNAMIC

---
+++ js/main.js
@@ -0,0 +1,34 @@
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    loadData();
+}
+async function loadData() {
+    const response = await fetch(
+        "https://designslayer.dk/portfolio/wp-json/wp/v2/bike?_embed"
+    );
+    console.log("response2", response);
+    const bikeData = await response.json();
+    displaybike(bikeData);
+}
+async function displaybike(userJSON) {
+    userJSON.forEach((bikes) => {
+        const template = document.querySelector("#bikeProductTemplate").content;
+        const copy = template.cloneNode(true);
+        copy.querySelector(".brand").textContent = bikes.brand;
+        copy.querySelector(".name").textContent = bikes.title.rendered;
+        copy.querySelector(".item-price").textContent = bikes.price + " $ ";
+        copy.querySelector(".availability").textContent = bikes.availability;
+        copy.querySelector(".color1").style.backgroundColor = bikes.color;
+        copy.querySelector(".color2").style.backgroundColor = bikes.color2;
+        copy.querySelector(".color3").style.backgroundColor = bikes.color3;
+        copy.querySelector(".color4").style.backgroundColor = bikes.color4;
+        copy.querySelector(".color5").style.backgroundColor = bikes.color5;
+
+        /*         copy.querySelector("bike-image").setAttribute();
+         */
+        copy.querySelector(".bike-image").src = bikes.image.guid;
+        const parent = document.querySelector("article");
+        parent.appendChild(copy);
+    });
+}
